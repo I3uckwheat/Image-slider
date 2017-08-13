de126f0fdbda039045247f29d810689036ff7513
Briggs Elsperger <briggse@protonmail.com>
2017-08-13 18:21:40 -0500

    removed all var's

--- js/slider.js
+++ js/slider.js
@@ -3,4 +3,17 @@ sliderNext = 2;
 
 $(document).ready(function(){
   $("#slider > img#1").fadeIn(300);
+  startSlider();
 });
+
+function startSlider() {
+  count = $("#slider > img").length;
+
+  loop = setInterval(function () {
+    $("slider > img").fadeOut(300);
+    $("slider > img#" + sliderNext).fadeIn(300);
+
+    sliderInt = sliderNext;
+    sliderNext = sliderNext + 1;
+  },3000)
+}
