158540e4f9366db1f22740b1c049f7dc3c25cc38
Briggs Elsperger <briggse@protonmail.com>
2017-08-13 17:50:21 -0500

    fixed jquery loading

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,6 @@
 sliderInt = 1;
 sliderNext = 2;
 
-$(document).ready(function () {
-  $("#slider>img#1").fadeIn(300);
-  alert("testing");
+$(document).ready(function(){
+  $("#slider > img#1").fadeIn(300);
 });
