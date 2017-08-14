64a1e4a6dfbf76df40100aab5f4422bce1c3f83c
Briggs Elsperger <briggse@protonmail.com>
2017-08-13 20:07:21 -0500

    added hover and next/prev buttons

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 sliderInt = 1;
 sliderNext = 2;
 
-$(document).ready(function(){
+$(document).ready(function () {
   $("#slider > img#1").fadeIn(300);
   startSlider();
 });
@@ -21,5 +21,47 @@ function startSlider() {
 
     sliderInt = sliderNext;
     sliderNext = sliderNext + 1;
-  },3000)
+  }, 3000)
+
 }
+
+function prev() {
+  newSlide = sliderInt - 1;
+  showSlide(newSlide);
+}
+
+function next() {
+  newSlide = sliderInt + 1;
+  showSlide(newSlide);
+}
+
+function stopLoop() {
+  window.clearInterval(loop);
+}
+
+function showSlide(id) {
+  stopLoop();
+  if (id > count) {
+    id = 1;
+  } else if (id < 1) {
+    id = count;
+  }
+
+  $("#slider > img").fadeOut(300);
+  $("#slider > img#" + sliderNext).fadeIn(300);
+
+
+  sliderInt = id;
+  sliderNext = id + 1;
+  startSlider();
+
+}
+
+$("#slider > img").hover(
+  function () {
+    stopLoop();
+  },
+  function () {
+    startSlider();
+  }
+);
